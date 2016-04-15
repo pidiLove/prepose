d2080f3b1e0bd00bbe788286aea85b3e2caf56e6
suiyuehanhao <1278808836@qq.com>
2016-04-15 18:06:52 +0800

    section1.practice-3

---
+++ practice-3.js
@@ -0,0 +1,13 @@
+﻿function collect_same_elements(collection_a, collection_b) {
+ 
+var result=[];
+for(var i=0;i<collection_a.length;i++)
+	for(var j=0;j<4;j++){
+		if(collection_a[i]==collection_b["value"][j])
+			result.push(collection_a[i]);
+
+
+}
+return result;
+ //在这里写入代码
+}
